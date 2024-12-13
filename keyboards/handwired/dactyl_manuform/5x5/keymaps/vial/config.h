57d0fcce459794c1f2798cacc1995f8d6f3cc070
Josh Dowd <jd@pop-waffles.lan>
2024-12-13 14:36:38 -0600

    added 5x5

---
+++ keyboards/handwired/dactyl_manuform/5x5/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#define VIAL_KEYBOARD_UID {0x91, 0x7D, 0xD5, 0x8D, 0x90, 0xB3, 0xB4, 0xB3}
+
+
+#define EE_HANDS
